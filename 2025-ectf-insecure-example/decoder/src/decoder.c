4a2b0d25a9194a6e78d00a3eff9aae45d16bd670
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-02-26 19:57:30 -0600

    Update decoder.c

--- 2025-ectf-insecure-example/decoder/src/decoder.c
+++ 2025-ectf-insecure-example/decoder/src/decoder.c
@@ -1,17 +1,12 @@
 /**
  * @file    decoder.c
- * @author  Samuel Meyers
- * @brief   eCTF Decoder Example Design Implementation
+ * @author  Dream Team
+ * @brief   eCTF Decoder Dream Team Design Implementation
  * @date    2025
- *
- * This source file is part of an example system for MITRE's 2025 Embedded System CTF (eCTF).
- * This code is being provided only for educational purposes for the 2025 MITRE eCTF competition,
- * and may not meet MITRE standards for quality. Use this code at your own risk!
- *
- * @copyright Copyright (c) 2025 The MITRE Corporation
  */
 
 /*********************** INCLUDES *************************/
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -21,20 +16,8 @@
 #include "mxc_delay.h"
 #include "simple_flash.h"
 #include "host_messaging.h"
-
 #include "simple_uart.h"
-
-/* Code between this #ifdef and the subsequent #endif will
-*  be ignored by the compiler if CRYPTO_EXAMPLE is not set in
-*  the projectk.mk file. */
-#ifdef CRYPTO_EXAMPLE
-/* The simple crypto example included with the reference design is intended
-*  to be an example of how you *may* use cryptography in your design. You
-*  are not limited nor required to use this interface in your design. It is
-*  recommended for newer teams to start by only using the simple crypto
-*  library until they have a working design. */
-#include "simple_crypto.h"
-#endif  //CRYPTO_EXAMPLE
+#include "simple_crypto.h"  // Ensure we have HMAC functionality
 
 /**********************************************************
  ******************* PRIMITIVE TYPES **********************
@@ -52,29 +35,22 @@
 #define MAX_CHANNEL_COUNT 8
 #define EMERGENCY_CHANNEL 0
 #define FRAME_SIZE 64
+#define HMAC_SIZE 32  // HMAC-SHA256 output size
 #define DEFAULT_CHANNEL_TIMESTAMP 0xFFFFFFFFFFFFFFFF
-// This is a canary value so we can confirm whether this decoder has booted before
 #define FLASH_FIRST_BOOT 0xDEADBEEF
-
-/**********************************************************
- ********************* STATE MACROS ***********************
- **********************************************************/
-
-// Calculate the flash address where we will store channel info as the 2nd to last page available
-#define FLASH_STATUS_ADDR ((MXC_FLASH_MEM_BASE + MXC_FLASH_MEM_SIZE) - (2 * MXC_FLASH_PAGE_SIZE))
-
+#define FLASH_SECRET_ADDR ((MXC_FLASH_MEM_BASE + MXC_FLASH_MEM_SIZE) - (3 * MXC_FLASH_PAGE_SIZE)) // Store secrets here
 
 /**********************************************************
  *********** COMMUNICATION PACKET DEFINITIONS *************
  **********************************************************/
 
-#pragma pack(push, 1) // Tells the compiler not to pad the struct members
-// for more information on what struct padding does, see:
-// https://www.gnu.org/software/c-intro-and-ref/manual/html_node/Structure-Layout.html
+#pragma pack(push, 1)
+
 typedef struct {
     channel_id_t channel;
     timestamp_t timestamp;
     uint8_t data[FRAME_SIZE];
+    uint8_t hmac[HMAC_SIZE];
 } frame_packet_t;
 
 typedef struct {
@@ -82,6 +58,7 @@ typedef struct {
     timestamp_t start_timestamp;
     timestamp_t end_timestamp;
     channel_id_t channel;
+    uint8_t hmac[HMAC_SIZE];
 } subscription_update_packet_t;
 
 typedef struct {
@@ -95,7 +72,7 @@ typedef struct {
     channel_info_t channel_info[MAX_CHANNEL_COUNT];
 } list_response_t;
 
-#pragma pack(pop) // Tells the compiler to resume padding struct members
+#pragma pack(pop)
 
 /**********************************************************
  ******************** TYPE DEFINITIONS ********************
@@ -109,7 +86,7 @@ typedef struct {
 } channel_status_t;
 
 typedef struct {
-    uint32_t first_boot; // if set to FLASH_FIRST_BOOT, device has booted before.
+    uint32_t first_boot;
     channel_status_t subscribed_channels[MAX_CHANNEL_COUNT];
 } flash_entry_t;
 
@@ -117,25 +94,22 @@ typedef struct {
  ************************ GLOBALS *************************
  **********************************************************/
 
-// This is used to track decoder subscriptions
 flash_entry_t decoder_status;
-
+uint8_t secret_key[32];  // Store the secret key extracted from flash
 
 /**********************************************************
  ******************* UTILITY FUNCTIONS ********************
  **********************************************************/
 
-/** @brief Checks whether the decoder is subscribed to a given channel
- *
- *  @param channel The channel number to be checked.
- *  @return 1 if the the decoder is subscribed to the channel.  0 if not.
-*/
+void load_secret_key() {
+    flash_simple_read(FLASH_SECRET_ADDR, secret_key, 32);
+}
+
+/** @brief Checks whether the decoder is subscribed to a given channel */
 int is_subscribed(channel_id_t channel) {
-    // Check if this is an emergency broadcast message
     if (channel == EMERGENCY_CHANNEL) {
         return 1;
     }
-    // Check if the decoder has has a subscription
     for (int i = 0; i < MAX_CHANNEL_COUNT; i++) {
         if (decoder_status.subscribed_channels[i].id == channel && decoder_status.subscribed_channels[i].active) {
             return 1;
@@ -144,60 +118,42 @@ int is_subscribed(channel_id_t channel) {
     return 0;
 }
 
+/** @brief Verifies the HMAC signature of an incoming frame */
+int verify_hmac(frame_packet_t *frame) {
+    uint8_t computed_hmac[HMAC_SIZE];
+    hmac_sha256(secret_key, 32, (uint8_t *)frame, sizeof(frame_packet_t) - HMAC_SIZE, computed_hmac);
 
+    if (memcmp(frame->hmac, computed_hmac, HMAC_SIZE) == 0) {
+        return 1;
+    } else {
+        print_error("HMAC verification failed\n");
+        return 0;
+    }
+}
 
 /**********************************************************
  ********************* CORE FUNCTIONS *********************
  **********************************************************/
 
-/** @brief Lists out the actively subscribed channels over UART.
- *
- *  @return 0 if successful.
-*/
-int list_channels() {
-    list_response_t resp;
-    pkt_len_t len;
-
-    resp.n_channels = 0;
-
-    for (uint32_t i = 0; i < MAX_CHANNEL_COUNT; i++) {
-        if (decoder_status.subscribed_channels[i].active) {
-            resp.channel_info[resp.n_channels].channel =  decoder_status.subscribed_channels[i].id;
-            resp.channel_info[resp.n_channels].start = decoder_status.subscribed_channels[i].start_timestamp;
-            resp.channel_info[resp.n_channels].end = decoder_status.subscribed_channels[i].end_timestamp;
-            resp.n_channels++;
-        }
-    }
-
-    len = sizeof(resp.n_channels) + (sizeof(channel_info_t) * resp.n_channels);
-
-    // Success message
-    write_packet(LIST_MSG, &resp, len);
-    return 0;
-}
-
-
-/** @brief Updates the channel subscription for a subset of channels.
- *
- *  @param pkt_len The length of the incoming packet
- *  @param update A pointer to an array of channel_update structs,
- *      which contains the channel number, start, and end timestamps
- *      for each channel being updated.
- *
- *  @note Take care to note that this system is little endian.
- *
- *  @return 0 upon success.  -1 if error.
-*/
+/** @brief Updates the channel subscription */
 int update_subscription(pkt_len_t pkt_len, subscription_update_packet_t *update) {
     int i;
 
     if (update->channel == EMERGENCY_CHANNEL) {
         STATUS_LED_RED();
-        print_error("Failed to update subscription - cannot subscribe to emergency channel\n");
+        print_error("Cannot subscribe to emergency channel\n");
         return -1;
     }
 
-    // Find the first empty slot in the subscription array
+    // Verify HMAC of subscription update
+    uint8_t computed_hmac[HMAC_SIZE];
+    hmac_sha256(secret_key, 32, (uint8_t *)update, sizeof(subscription_update_packet_t) - HMAC_SIZE, computed_hmac);
+    if (memcmp(update->hmac, computed_hmac, HMAC_SIZE) != 0) {
+        STATUS_LED_RED();
+        print_error("Subscription HMAC invalid\n");
+        return -1;
+    }
+
     for (i = 0; i < MAX_CHANNEL_COUNT; i++) {
         if (decoder_status.subscribed_channels[i].id == update->channel || !decoder_status.subscribed_channels[i].active) {
             decoder_status.subscribed_channels[i].active = true;
@@ -208,206 +164,94 @@ int update_subscription(pkt_len_t pkt_len, subscription_update_packet_t *update)
         }
     }
 
-    // If we do not have any room for more subscriptions
     if (i == MAX_CHANNEL_COUNT) {
         STATUS_LED_RED();
-        print_error("Failed to update subscription - max subscriptions installed\n");
+        print_error("Max subscriptions reached\n");
         return -1;
     }
 
     flash_simple_erase_page(FLASH_STATUS_ADDR);
     flash_simple_write(FLASH_STATUS_ADDR, &decoder_status, sizeof(flash_entry_t));
-    // Success message with an empty body
     write_packet(SUBSCRIBE_MSG, NULL, 0);
     return 0;
 }
 
-/** @brief Processes a packet containing frame data.
- *
- *  @param pkt_len A pointer to the incoming packet.
- *  @param new_frame A pointer to the incoming packet.
- *
- *  @return 0 if successful.  -1 if data is from unsubscribed channel.
-*/
+/** @brief Processes a frame packet */
 int decode(pkt_len_t pkt_len, frame_packet_t *new_frame) {
     char output_buf[128] = {0};
-    uint16_t frame_size;
-    channel_id_t channel;
+    uint16_t frame_size = pkt_len - (sizeof(new_frame->channel) + sizeof(new_frame->timestamp) + HMAC_SIZE);
+    channel_id_t channel = new_frame->channel;
 
-    // Frame size is the size of the packet minus the size of non-frame elements
-    frame_size = pkt_len - (sizeof(new_frame->channel) + sizeof(new_frame->timestamp));
-    channel = new_frame->channel;
-
-    // The reference design doesn't use the timestamp, but you may want to in your design
-    // timestamp_t timestamp = new_frame->timestamp;
-
-    // Check that we are subscribed to the channel...
     print_debug("Checking subscription\n");
-    if (is_subscribed(channel)) {
-        print_debug("Subscription Valid\n");
-        /* The reference design doesn't need any extra work to decode, but your design likely will.
-        *  Do any extra decoding here before returning the result to the host. */
-        write_packet(DECODE_MSG, new_frame->data, frame_size);
-        return 0;
-    } else {
+    if (!is_subscribed(channel)) {
         STATUS_LED_RED();
-        sprintf(
-            output_buf,
-            "Receiving unsubscribed channel data.  %u\n", channel);
+        sprintf(output_buf, "Unsubscribed channel: %u\n", channel);
         print_error(output_buf);
         return -1;
     }
+
+    if (!verify_hmac(new_frame)) {
+        STATUS_LED_RED();
+        print_error("Frame HMAC verification failed\n");
+        return -1;
+    }
+
+    write_packet(DECODE_MSG, new_frame->data, frame_size);
+    return 0;
 }
 
-/** @brief Initializes peripherals for system boot.
-*/
+/**********************************************************
+ ************************ MAIN ****************************
+ **********************************************************/
+
 void init() {
-    int ret;
-
-    // Initialize the flash peripheral to enable access to persistent memory
     flash_simple_init();
+    load_secret_key();
 
-    // Read starting flash values into our flash status struct
     flash_simple_read(FLASH_STATUS_ADDR, &decoder_status, sizeof(flash_entry_t));
     if (decoder_status.first_boot != FLASH_FIRST_BOOT) {
-        /* If this is the first boot of this decoder, mark all channels as unsubscribed.
-        *  This data will be persistent across reboots of the decoder. Whenever the decoder
-        *  processes a subscription update, this data will be updated.
-        */
-        print_debug("First boot.  Setting flash...\n");
-
         decoder_status.first_boot = FLASH_FIRST_BOOT;
-
-        channel_status_t subscription[MAX_CHANNEL_COUNT];
-
-        for (int i = 0; i < MAX_CHANNEL_COUNT; i++){
-            subscription[i].start_timestamp = DEFAULT_CHANNEL_TIMESTAMP;
-            subscription[i].end_timestamp = DEFAULT_CHANNEL_TIMESTAMP;
-            subscription[i].active = false;
-        }
-
-        // Write the starting channel subscriptions into flash.
-        memcpy(decoder_status.subscribed_channels, subscription, MAX_CHANNEL_COUNT*sizeof(channel_status_t));
-
+        memset(decoder_status.subscribed_channels, 0, sizeof(decoder_status.subscribed_channels));
         flash_simple_erase_page(FLASH_STATUS_ADDR);
         flash_simple_write(FLASH_STATUS_ADDR, &decoder_status, sizeof(flash_entry_t));
     }
 
-    // Initialize the uart peripheral to enable serial I/O
-    ret = uart_init();
-    if (ret < 0) {
+    if (uart_init() < 0) {
         STATUS_LED_ERROR();
-        // if uart fails to initialize, do not continue to execute
         while (1);
     }
 }
 
-/* Code between this #ifdef and the subsequent #endif will
-*  be ignored by the compiler if CRYPTO_EXAMPLE is not set in
-*  the projectk.mk file. */
-#ifdef CRYPTO_EXAMPLE
-void crypto_example(void) {
-    // Example of how to utilize included simple_crypto.h
-
-    // This string is 16 bytes long including null terminator
-    // This is the block size of included symmetric encryption
-    char *data = "Crypto Example!";
-    uint8_t ciphertext[BLOCK_SIZE];
-    uint8_t key[KEY_SIZE];
-    uint8_t hash_out[HASH_SIZE];
-    uint8_t decrypted[BLOCK_SIZE];
-
-    char output_buf[128] = {0};
-
-    // Zero out the key
-    bzero(key, BLOCK_SIZE);
-
-    // Encrypt example data and print out
-    encrypt_sym((uint8_t*)data, BLOCK_SIZE, key, ciphertext);
-    print_debug("Encrypted data: \n");
-    print_hex_debug(ciphertext, BLOCK_SIZE);
-
-    // Hash example encryption results
-    hash(ciphertext, BLOCK_SIZE, hash_out);
-
-    // Output hash result
-    print_debug("Hash result: \n");
-    print_hex_debug(hash_out, HASH_SIZE);
-
-    // Decrypt the encrypted message and print out
-    decrypt_sym(ciphertext, BLOCK_SIZE, key, decrypted);
-    sprintf(output_buf, "Decrypted message: %s\n", decrypted);
-    print_debug(output_buf);
-}
-#endif  //CRYPTO_EXAMPLE
-
-/**********************************************************
- *********************** MAIN LOOP ************************
- **********************************************************/
-
 int main(void) {
-    char output_buf[128] = {0};
-    uint8_t uart_buf[100];
-    msg_type_t cmd;
-    int result;
-    uint16_t pkt_len;
-
-    // initialize the device
     init();
-
     print_debug("Decoder Booted!\n");
 
-    // process commands forever
     while (1) {
-        print_debug("Ready\n");
-
         STATUS_LED_GREEN();
+        msg_type_t cmd;
+        uint8_t uart_buf[100];
+        uint16_t pkt_len;
 
-        result = read_packet(&cmd, uart_buf, &pkt_len);
-
-        if (result < 0) {
+        if (read_packet(&cmd, uart_buf, &pkt_len) < 0) {
             STATUS_LED_ERROR();
-            print_error("Failed to receive cmd from host\n");
+            print_error("Failed to receive command\n");
             continue;
         }
 
-        // Handle the requested command
         switch (cmd) {
-
-        // Handle list command
-        case LIST_MSG:
-            STATUS_LED_CYAN();
-
-            #ifdef CRYPTO_EXAMPLE
-                // Run the crypto example
-                // TODO: Remove this from your design
-                crypto_example();
-            #endif // CRYPTO_EXAMPLE
-
-            // Print the boot flag
-            // TODO: Remove this from your design
-            boot_flag();
-            list_channels();
-            break;
-
-        // Handle decode command
-        case DECODE_MSG:
-            STATUS_LED_PURPLE();
-            decode(pkt_len, (frame_packet_t *)uart_buf);
-            break;
-
-        // Handle subscribe command
-        case SUBSCRIBE_MSG:
-            STATUS_LED_YELLOW();
-            update_subscription(pkt_len, (subscription_update_packet_t *)uart_buf);
-            break;
-
-        // Handle bad command
-        default:
-            STATUS_LED_ERROR();
-            sprintf(output_buf, "Invalid Command: %c\n", cmd);
-            print_error(output_buf);
-            break;
+            case LIST_MSG:
+                list_channels();
+                break;
+            case DECODE_MSG:
+                decode(pkt_len, (frame_packet_t *)uart_buf);
+                break;
+            case SUBSCRIBE_MSG:
+                update_subscription(pkt_len, (subscription_update_packet_t *)uart_buf);
+                break;
+            default:
+                print_error("Invalid command\n");
+                break;
         }
     }
 }
+
